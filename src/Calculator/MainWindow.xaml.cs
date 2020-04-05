bd6632354ac862339e5b98a6e6816704d71045fc
Václavík Marek <xvacla26@vutbr.cz>
2020-04-05 22:36:05 +0200

    On DesktopApp: Vzhled aplikace, propojení UI a matematické knihovny + materialDesign

--- src/Calculator/MainWindow.xaml.cs
+++ src/Calculator/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MathLib;
+using MathLib.Expression;
+using MathLib.Exceptions;
+
 
 namespace Calculator
 {
@@ -20,9 +24,163 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IMathLibrary Library { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Library = MathLibraryFactory.Build();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+        private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Window_Minimalize(object sender, RoutedEventArgs e)
+        {
+            MyWindow.WindowState = WindowState.Minimized;
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void N_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += ((Button)sender).Content.ToString();
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Delete_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text = "";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Pi_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "π";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Euler_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "e";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Equal_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var parser = new MathLib.Expression.Parser(Expresion_TextBox.Text);
+            decimal v = parser.Evaluate();
+            Answer_TextBox.Text = v.ToString();
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Plus_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "+";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Minus_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "-";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Multiply_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "*";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Divide_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "/";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Left_Bracket_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "(";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Right_Bracket_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += ")";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Factorial_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "!";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Modulo_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "%";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Root_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "√";
+        }
+        /// <summary>
+		///     Handle numeric button click.
+		/// </summary>
+		/// <param name="sender">Sender object.</param>
+		/// <param name="e">RoutedEventArgs.</param>
+		private void Power_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expresion_TextBox.Text += "^";
         }
     }
 }
